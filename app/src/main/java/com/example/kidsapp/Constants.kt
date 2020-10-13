b91310535b6aecaf2f2a17b45d832d017128eb2e
Tejaswini <tejaswini.bs7@gmail.com>
2020-10-13 14:29:08 +0200

    Test Question Added

--- app/src/main/java/com/example/kidsapp/Constants.kt
+++ app/src/main/java/com/example/kidsapp/Constants.kt
@@ -2,5 +2,17 @@ package com.example.kidsapp
 
 object Constants {
    const val USERNAME:String="user_name"
+   const val TOTALQUESTIONS:String="total_questions"
+   const val CORRECTANSWER:String="correct_answer"
+
+   fun getQuestions():ArrayList<Question>
+   {
+      val QuestionList=ArrayList<Question>()
+      var question1=Question(1,"Identify the Animal",R.drawable.tiger,"Lion","cat",
+         "Tiger","Dog",3)
+      QuestionList.add(question1)
+      return QuestionList
+   }
+
 
 }
--- app/src/main/java/com/example/kidsapp/QuestionsActivity.kt
+++ app/src/main/java/com/example/kidsapp/QuestionsActivity.kt
@@ -5,7 +5,12 @@ import android.os.Bundle
 
 class QuestionsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+       lateinit var mQuestionList:ArrayList<Question>
+        lateinit var musername: String?
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_questions)
+        musername=intent.getStringExtra(Constants.USERNAME)
+       mQuestionList= Constants.getQuestions()
+
     }
 }
