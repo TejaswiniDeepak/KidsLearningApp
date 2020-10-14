2cbb1bfde238ca99e372e9669cafce0ebd4f24ef
Tejaswini <tejaswini.bs7@gmail.com>
2020-10-14 13:32:17 +0200

    2nd update

--- app/src/main/java/com/example/kidsapp/Constants.kt
+++ app/src/main/java/com/example/kidsapp/Constants.kt
@@ -3,7 +3,7 @@ package com.example.kidsapp
 object Constants {
    const val USERNAME:String="user_name"
    const val TOTALQUESTIONS:String="total_questions"
-   const val CORRECTANSWER:String="correct_answer"
+   const val CORRECTANSWER:Int=0
 
    fun getQuestions():ArrayList<Question>
    {
@@ -11,6 +11,9 @@ object Constants {
       var question1=Question(1,"Identify the Animal",R.drawable.tiger,"Lion","cat",
          "Tiger","Dog",3)
       QuestionList.add(question1)
+      var question2=Question(2,"Identify the Animal",R.drawable.tiger,"Lion","cat",
+         "Tiger","Dog",4)
+      QuestionList.add(question2)
       return QuestionList
    }
 
--- app/src/main/java/com/example/kidsapp/QuestionsActivity.kt
+++ app/src/main/java/com/example/kidsapp/QuestionsActivity.kt
@@ -1,15 +1,23 @@
 package com.example.kidsapp
 
+import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_questions.*
 
-class QuestionsActivity : AppCompatActivity() {
+class QuestionsActivity : AppCompatActivity(),View.OnClickListener {
     private var mQuestionList:ArrayList<Question>?=null
     private var musername: String?=null
     private var index:Int=1
+    private var mCorrectOption:Int=1
+    private var mSelectedOption:Int=0
+    private var QuestionCompleted:Boolean=false
+
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
@@ -17,27 +25,139 @@ class QuestionsActivity : AppCompatActivity() {
 
         musername=intent.getStringExtra(Constants.USERNAME)
        mQuestionList= Constants.getQuestions()
+        Log.i("size","${mQuestionList!!.size}")
 
         option1.setOnClickListener(this)
-
-
-        animal_image.setImageResource(R.drawable.tiger)
-        option1.text= mQuestionList!![index - 1].optionOne
-        option2.text= mQuestionList!![index - 1].optionTwo
-        option3.text= mQuestionList!![index - 1].optionThree
-        option4.text= mQuestionList!![index - 1].optionFour
-
+        option2.setOnClickListener(this)
+        option3.setOnClickListener(this)
+        option4.setOnClickListener(this)
+        btn_submit.setOnClickListener(this)
+        setQuestion()
 
 
     }
-    fun onClick(v: View?) {
-        when (v?.id) {
+
+    override fun onClick(p0: View?) {
+        when (p0?.id) {
             R.id.option1 -> {
-                Toast.makeText(this, "one one selected", Toast.LENGTH_SHORT).show()
+                originalView()
+                optionClicked(option1, 1)
+                Toast.makeText(this, "option1 clicked", Toast.LENGTH_SHORT).show()
             }
+
             R.id.option2 -> {
-                Toast.makeText(this, "one one selected", Toast.LENGTH_SHORT).show()
+                originalView()
+                optionClicked(option2, 2)
+                Toast.makeText(this, "option2 clicked", Toast.LENGTH_SHORT).show()
             }
+            R.id.option3 -> {
+                originalView()
+                optionClicked(option3, 3)
+                Toast.makeText(this, "option3 clicked", Toast.LENGTH_SHORT).show()
+            }
+            R.id.option4 -> {
+                originalView()
+
+                optionClicked(option4, 4)
+                Toast.makeText(this, "option4 clicked", Toast.LENGTH_SHORT).show()
+            }
+            R.id.btn_submit -> {
+                if (mSelectedOption == 0) {
+                    index++
+
+                    setQuestion()
+
+                    /**   if (index < mQuestionList!!.size) {
+                    index++
+                    btn_submit.text = "Next"
+                    setQuestion()
+                    } else {
+                    btn_submit.text = "Finish"
+
+                    }
+
+                     **/
+                }
+                    else {
+                    if (mSelectedOption == mCorrectOption) {
+                        correctAnswerView(mSelectedOption)
+                        QuestionCompleted=true
+
+
+                    } else {
+                        wrongAnswerView(mSelectedOption)
+                        correctAnswerView(mCorrectOption)
+                        QuestionCompleted=true
+                    }
+                    mSelectedOption = 0
+                    if(index<=mQuestionList!!.size)
+                    {
+                        btn_submit.text="next"
+                    }
+                    else
+                    {
+                        btn_submit.text="finish"
+                    }
+                }
+
+
+
+            }
+
         }
     }
+
+
+    fun optionClicked(btn:Button,selectedOption:Int)
+    {
+      mSelectedOption=selectedOption
+
+        btn.setBackgroundResource(R.drawable.btn_bg)
+        btn.setTextColor(Color.parseColor("#6C9E9C"))
+    }
+
+    fun originalView()
+    {
+
+        option1.setBackgroundColor(Color.parseColor("#FFFFFF"))
+        option1.setTextColor(Color.parseColor("#050A0A"))
+        option2.setBackgroundColor(Color.parseColor("#FFFFFF"))
+        option2.setTextColor(Color.parseColor("#050A0A"))
+        option3.setBackgroundColor(Color.parseColor("#FFFFFF"))
+        option3.setTextColor(Color.parseColor("#050A0A"))
+        option4.setBackgroundColor(Color.parseColor("#FFFFFF"))
+        option4.setTextColor(Color.parseColor("#050A0A"))
+    }
+    fun correctAnswerView(selectedOption: Int)
+    {
+when(selectedOption)
+{
+    1->option1.setBackgroundColor(Color.parseColor("#8CEC73"))
+    2->option2.setBackgroundColor(Color.parseColor("#8CEC73"))
+    3->option3.setBackgroundColor(Color.parseColor("#8CEC73"))
+    4->option4.setBackgroundColor(Color.parseColor("#8CEC73"))
+}
+    }
+    fun wrongAnswerView(selectedOption: Int)
+    {
+        when(selectedOption)
+        {
+            1->option1.setBackgroundColor(Color.parseColor("#E1516E"))
+            2->option2.setBackgroundColor(Color.parseColor("#E1516E"))
+            3->option3.setBackgroundColor(Color.parseColor("#E1516E"))
+            4->option4.setBackgroundColor(Color.parseColor("#E1516E"))
+        }
+    }
+    fun setQuestion()
+    {
+        var Question=mQuestionList!![index-1]
+        animal_image.setImageResource(Question.image)
+        option1.text= Question.optionOne
+        option2.text= Question.optionTwo
+        option3.text= Question.optionThree
+        option4.text= Question.optionFour
+        mCorrectOption=Question.correctAnswer
+
+    }
+
 }
